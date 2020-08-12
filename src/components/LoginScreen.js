fa562a4e79f9e88e09c0da29c06d5597ad3eb67c
Fabio Mantelli <fmmantelli@gmail.com>
2020-08-12 11:18:54 -0300

    refactor(styled-loginscreen): removed CSS to LoginScreen and refactored to styled-components

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,25 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import '../styles/LoginScreen.css';
+
+// styled-components 
+
+const Text = styled.h1`
+    font-size: 1rem;
+`
+
+const Input = styled.input`
+    margin-bottom: 10px; 
+    width: 300px;
+	background: rgba(0,0,0,0.1);
+	border: none;
+	outline: none;
+	padding: 10px;
+	font-size: 13px;
+	border: 1px solid rgba(0,0,0,0.3);
+    border-radius: 4px;
+    
+    &:focus {
+        box-shadow: inset 0 -5px 45px rgba(100,100,100,0.4), 0 1px 1px rgba(255,255,255,0.2); 
+    }
+`
+
+const LoginButton = styled.input`
+    padding: 10px;
+    width: 330px;
+    cursor: pointer;
+    line-height: 20px;
+    border-radius: 8px;
+    background-color: #1D4D6B;
+    border-color: #34759e;
+    color: white;
+`
 
 export default function Login(props) {
     return (
         <div className="login">
             <div>
-                <h1>Username</h1>
-                <input className="input-login" type="text" {...props.username} autoComplete="new-password" />
+                <Text>Username</Text>
+                <Input type="text" {...props.username} autoComplete="new-password" />
             </div>
 
             <div>
-                <h1>Password</h1>
-                <input className="input-login" type="password" {...props.password} autoComplete="new-password" />
+                <Text>Password</Text>
+                <Input type="password" {...props.password} autoComplete="new-password" />
             </div>
             
             <div>
                 {props.error && <><small style={{ color: 'red' }}>Login or password invalid.</small><br /></>}<br />
             </div>
 
-            <input className="btn" type="button" value={props.loading ? 'Loading...' : 'Login'} onClick={props.handleLogin} disabled={props.loading} />
+            <LoginButton className="btn" type="button" value={props.loading ? 'Loading...' : 'Login'} onClick={props.handleLogin} disabled={props.loading} />
         </div>
       
     )
